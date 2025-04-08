0087f8d4dcfc12305b25f7c1218d7f984c09e0a9
Ecadra <edenrulezzz@gmail.com>
2025-04-07 20:12:45 -0600

    Codigo bonito

--- Scripts/kmeans_procedures/Transformation_Loading_Process.py
+++ Scripts/kmeans_procedures/Transformation_Loading_Process.py
@@ -1,15 +1,13 @@
 import os
-from numpy import size
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 
-def leer_nombres(ruta_nombres):
-
-    with open(ruta_nombres,'r') as file:
-        lectura = file.readlines()
-        column_names = []
-        for line in lectura:
+def read_names(path_names:str) -> list[str]:   
+    with open(path_names,'r') as file:
+        read = file.readlines()
+        column_names:list[str] = []
+        for line in read:
             if line.startswith('|') or line.startswith('\n') :
                 #ignorar comentarios y nuevas lineas
                 continue
@@ -19,60 +17,57 @@ def leer_nombres(ruta_nombres):
                     column_names.append(column_parts[0].strip())
     return column_names
 
-def filtrar_dataset(ruta_data,ruta_csv,nombres_columnas,columnas_deseadas):
-    if os.path.exists(ruta_csv):
-        return pd.read_csv(ruta_csv,header = 0,sep = ",")
+def filter_dataset(raw_csv_input:str,original_column_names:list[str],filtered_csv_output_path:str,filtered_column_names:list[str]) -> pd.DataFrame:
+    if os.path.exists(filtered_csv_output_path):
+        filtered_df:pd.DataFrame = pd.read_csv(filtered_csv_output_path,header = 0,sep = ",")
+        return filtered_df
     else:
-        nombres_columnas = leer_nombres(ruta_name)
-        data = pd.read_csv(ruta_data, 
+        original_column_names = read_names(path_names)
+        data:pd.DataFrame = pd.read_csv(raw_csv_input, 
                                 header=None, 
                                 sep = ",",
                                 engine = 'python',
                                 na_values=[' ?',""," Not in universe"," Not in universe or children"," Not in universe under 1 year old"])
-        data.columns = nombres_columnas
+        data.columns = original_column_names
         #Filtrado de las columnas que nos interesan
-        data = data[columnas_deseadas]
+        data = data[filtered_column_names]
         data = data.drop_duplicates()
         data = data.dropna()
-        data.to_csv(ruta_csv,index=False)
+        data.to_csv(filtered_csv_output_path,index=False)
     return data
 
-def normalizar_dataset(data_frame, ruta_salida):
-    if os.path.exists(ruta_salida):
-        return pd.read_csv(ruta_salida,header = 0,sep = ",")
+def normalize_dataset(filtered_df:pd.DataFrame, output_path:str) -> pd.DataFrame:
+    if os.path.exists(output_path):
+        return pd.read_csv(output_path,header = 0,sep = ",")
     else:
-        numeric_columns = data_frame.select_dtypes(include=['int64','float64']).columns
-        categorical_columns = data_frame.select_dtypes(include=['object']).columns
-        #scaler = StandardScaler()
-        #StandardScaler utiliza la media y la desviación estándar, por lo que la normalización puede involucrar numeros negativos
-        #? Para K-means, el adecuado a utilizar es MinMaxScaler, porque simplifica el procesado de clusterización.
-        scaler = MinMaxScaler()
-        data_frame[numeric_columns] = scaler.fit_transform(data_frame[numeric_columns])
         le = LabelEncoder()
+        scaler = MinMaxScaler()
+        categorical_columns:pd.Index[str] = filtered_df.select_dtypes(include=['object']).columns
         for col in categorical_columns:
-            data_frame[col] = le.fit_transform(data_frame[col])
+            filtered_df[col] = le.fit_transform(filtered_df[col])
+        numeric_columns:pd.Index[str] = filtered_df.select_dtypes(include=['int64','float64']).columns
+        filtered_df[numeric_columns] = scaler.fit_transform(filtered_df[numeric_columns])
+        filtered_df.to_csv(output_path,index = False)
+    return filtered_df
 
-        data_frame.to_csv(ruta_salida,index = False)
-    return data_frame
+path_data = "./Data/extracted_data/tarFile/census-income.data"
+path_names = "./Data/extracted_data/tarFile/census-income.names"
+path_test = "./Data/extracted_data/tarFile/census-income.test"
 
-ruta_data = "./Data/extracted_data/tarFile/census-income.data"
-ruta_name = "./Data/extracted_data/tarFile/census-income.names"
-ruta_test = "./Data/extracted_data/tarFile/census-income.test"
+path_filtered = "./Data/extracted_data/census-filtered.csv"
+selected_columns =["age","class of worker","education","major industry code","major occupation code","race","sex","country of birth father","country of birth mother","country of birth self","citizenship","own business or self employed","yearly income"]
 
-ruta_csv_filtrado = "./Data/extracted_data/census-filtered.csv"
-lista_columnas_deseadas =["age","class of worker","education","major industry code","major occupation code","race","sex","country of birth father","country of birth mother","country of birth self","citizenship","own business or self employed","yearly income"]
+df = filter_dataset(path_data,read_names(path_names),path_filtered,selected_columns)
 
-data_frame = filtrar_dataset(ruta_data,ruta_csv_filtrado,leer_nombres(ruta_name),lista_columnas_deseadas)
-
-print(data_frame.head())
+print(df.head())
 
 #Comprobación de que ya no hay nulos:
-print(f'El total de valores nulos por columna es: {data_frame.isnull().sum()}')
+print(f'El total de valores nulos por columna es: {df.isnull().sum()}')
 
-ruta_dataset_normalizado = "./Data/extracted_data/census-normalized.csv"
-data_frame = normalizar_dataset(data_frame,ruta_dataset_normalizado)
+path_normalized_df = "./Data/extracted_data/census-normalized.csv"
+df = normalize_dataset(df,path_normalized_df)
 
-print(data_frame.head())
-print(data_frame.describe)
+print(df.head())
+print(df.describe)
 
-
+print(df.dtypes)
